fd35c78c3f413cc6e9d0ef51eee64d53d69a6228
Isabella Gomez <isabellagomezhj@gmail.com>
2023-11-26 23:35:15 -0500

    Project so far--still grappling with notification issue

--- HapticDance/AppDelegate.swift
+++ HapticDance/AppDelegate.swift
@@ -5,32 +5,40 @@
 //  Created by Isabella Gomez on 11/16/23.
 //
 
+import Firebase
+//import FirebaseAuth
+import FirebaseCore
+import FirebaseFirestone
+import FirebaseMessaging
+import UserNotifications
 import UIKit
 import CoreHaptics
 import AVFoundation
 
+
 @main
-class AppDelegate: UIResponder, UIApplicationDelegate {
+class AppDelegate: UIResponder, UIApplicationDelegate, MessagingDelegate, UNUserNotificationCenterDelegate {
     
     var window: UIWindow?
     var supportsHaptics: Bool = true
     
-    func application(_ application: UIApplication, didFinishLaunchingWithOptions launchOptions: [UIApplication.LaunchOptionsKey: Any]?) -> Bool {
+    func application(_ application: UIApplication,
+                     didFinishLaunchingWithOptions launchOptions: [UIApplication.LaunchOptionsKey: Any]?) -> Bool {
         
-//        // Configure push notifications
-//        FirebaseApp.configure()
-//
-//        Messaging.messaging().delegate = self
-//        UNUserNotificationCenter.current().delegate = self
-//
-//        UNUserNotificationCenter.current().requestAuthorization(options: [.alert, .sound, .badge]) { success, _ in guard success else {
-//            return
-//        }
-//            print("Success in APNS registry")
-//        }
-//
-//        application.registerForRemoteNotifications()
-//
+        // Configure push notifications
+        FirebaseApp.configure()
+
+        Messaging.messaging().delegate = self
+        UNUserNotificationCenter.current().delegate = self
+
+        UNUserNotificationCenter.current().requestAuthorization(options: [.alert, .sound, .badge]) { success, _ in guard success else {
+            return
+        }
+            print("Success in APNS registry")
+        }
+
+        application.registerForRemoteNotifications()
+
         // Stop screen from sleeping
         UIApplication.shared.isIdleTimerDisabled = true
         
@@ -42,13 +50,22 @@ class AppDelegate: UIResponder, UIApplicationDelegate {
     }
 
 
-//    func messaging(_ messaging: Messaging, didReceiveRegistrationToken fcmToken: String?) {
-//        messaging.token { token, _ in
-//            guard let token = token else {
-//                return
-//            }
-//            print("Token: \(token)")
+    func messaging(_ messaging: Messaging, didReceiveRegistrationToken fcmToken: String?) {
+        messaging.token { token, _ in
+            guard let token = token else {
+                return
+            }
+            print("Token: \(token)")
+        }
+    }
+    
+//    func application(_ application: UIApplication,
+//                   didReceiveRemoteNotification userInfo: [AnyHashable : Any],
+//                   fetchCompletionHandler completionHandler: @escaping (UIBackgroundFetchResult) -> Void) {
+//        if let stringData = userInfo["startTime"] as? String {
+//          print("Time: \(stringData)")
 //        }
+//        completionHandler(.newData)
 //    }
 
 }
--- Podfile
+++ Podfile
@@ -1,5 +1,5 @@
 # Uncomment the next line to define a global platform for your project
-platform :ios, '14.0'
+  platform :ios, '15.0'
 
 target 'HapticDance' do
   # Comment the next line if you don't want to use dynamic frameworks
@@ -9,4 +9,12 @@ target 'HapticDance' do
   pod 'Firebase/Core'
   pod 'Firebase/Messaging'
 
+end
+
+post_install do |installer|
+  installer.pods_project.targets.each do |target|
+    target.build_configurations.each do |config|
+      config.build_settings.delete 'IPHONEOS_DEPLOYMENT_TARGET'
+    end
+  end
 end
